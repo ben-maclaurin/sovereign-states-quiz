79f1c956170f0ca03834667dfc94a272961a3d87
ben@thedesigncollaborative.co <ben@thedesigncollaborative.co>
2021-06-26 20:08:53 +0100

    Convert to web endpoint for Svelte app

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 #![feature(string_remove_matches)]
 use scraper::{Html, Selector};
-use warp::{Filter, http::Response}
 use std::io;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use warp::{Filter};
+use serde::{Serialize, Deserialize};
 
-type Country = String;
+#[derive(Serialize, Deserialize)]
+struct Country {
+    name: String
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    return match get_countries().await {
-        Ok(v) => {
-            start_test(parse_countries(v));
-            Ok(())
-        }
-        Err(_) => Err("bad request")?,
-    };
+async fn main() {
+
+    let route = warp::path("countries")
+        .map(|| {
+            warp::reply::json(&get_countries())
+        });
+
+        let server = warp::serve(route);
+
+        server.run(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080)).await;
 }
 
 async fn get_countries() -> Result<Vec<Country>, Box<dyn std::error::Error>> {
@@ -28,7 +35,7 @@ async fn get_countries() -> Result<Vec<Country>, Box<dyn std::error::Error>> {
     let selector = Selector::parse("span").unwrap();
 
     // Exclude some values
-    let mut countries = Vec::new();
+    let mut countries = Vec::<Country>::new();
     let exclusions = vec![
         "List_of_states",
         "Other_states",
@@ -45,14 +52,16 @@ async fn get_countries() -> Result<Vec<Country>, Box<dyn std::error::Error>> {
             Some(c) => {
                 // If we find an id match, check it against list of exclusions
                 if !exclusions.iter().any(|&v| v == c) {
-                    countries.push(String::from(c))
+                    countries.push(Country {
+                        name: String::from(c)
+                    })
                 }
             }
             None => {}
         }
     }
 
-    Ok(countries)
+    Ok(parse_countries(countries))
 }
 
 fn start_test(countries: Vec<Country>) -> io::Result<()> {
@@ -61,11 +70,11 @@ fn start_test(countries: Vec<Country>) -> io::Result<()> {
 
         io::stdin().read_line(&mut input)?;
 
-        if input.trim().to_lowercase() == country.to_lowercase() {
+        if input.trim().to_lowercase() == country.name.to_lowercase() {
             println!("✅ Correct {}/{}", i + 1, countries.len() - 1);
         } else {
             print!("{}[2J", 27 as char);
-            println!("❌ {:?} was the correct answer", country);
+            println!("❌ {:?} was the correct answer", country.name);
             break;
         }
     }
@@ -74,13 +83,17 @@ fn start_test(countries: Vec<Country>) -> io::Result<()> {
 }
 
 fn parse_countries(countries: Vec<Country>) -> Vec<Country> {
-    let mut countries_parsed = Vec::new();
+    let mut countries_parsed = Vec::<Country>::new();
 
     for country in countries {
-        if country.contains("_") {
-            countries_parsed.push(country.replace("_", " "));
+        if country.name.contains("_") {
+            countries_parsed.push(
+                Country {
+                    name: country.name.replace("_", " ")
+                }
+                );
         } else {
-            countries_parsed.push(String::from(country));
+            countries_parsed.push(country);
         }
     }
 
