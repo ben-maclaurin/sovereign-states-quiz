8a0d13b8f74977d1267b037a4812d2bbd499c5fc
ben@thedesigncollaborative.co <ben@thedesigncollaborative.co>
2021-06-27 13:17:28 +0100

    Pull in list of countries

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export async function load({ fetch }) {
+		const url = 'http://localhost:8080/countries'
+		const res = await fetch(url)
+
+		if (res.ok) {
+		  return {
+				props: {
+					countries: await res.json()
+				}
+			}
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${url}`)
+		}
+	}
+
+</script>
+
+<script>
+	export let countries;
+</script>
+
+
+<svelte:head>
+				<title>Welcome</title>
+</svelte:head>
+
+<ul>
+	{#each countries as country}
+		<li>{country.name}</li>
+	{/each}
+</ul>
+
+
