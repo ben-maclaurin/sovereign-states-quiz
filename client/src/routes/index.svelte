4c87c94aa5329b06022c55879df4732894a77581
ben@thedesigncollaborative.co <ben@thedesigncollaborative.co>
2021-07-01 16:03:08 +0100

    Reformat code

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,66 +1,67 @@
 <script context="module">
-	export async function load({ fetch }) {
-		const url = 'http://localhost:8080/countries'
-		const res = await fetch(url)
+    export async function load({fetch}) {
+        const url = 'http://localhost:8080/countries'
+        const res = await fetch(url)
 
-		if (res.ok) {
-		  return {
-				props: {
-					countries: await res.json()
-				}
-			}
-		}
+        if (res.ok) {
+            return {
+                props: {
+                    countries: await res.json()
+                }
+            }
+        }
 
-		return {
-			status: res.status,
-			error: new Error(`Could not load ${url}`)
-		}
-	}
+        return {
+            status: res.status,
+            error: new Error(`Could not load ${url}`)
+        }
+    }
 
 </script>
 
 <script>
-		import { writable } from "svelte/store"
-		export let countries;
+    import {writable} from "svelte/store"
 
-		let correct = writable([]) 
+    export let countries;
 
-		let response = '';
-		let count = 0;
-			let answer = ''
+    let correct = writable([])
+
+    let response = '';
+    let count = 0;
+    let answer = ''
 
 
-		const handleKeydown = (e) => {
-			  if (e.keyCode == 13 && response.toLowerCase() === countries[count].name.toLowerCase()) {
-						answer = ''
-					response = ''
-					$correct = [...$correct, countries[count].name];
-					count++
-				}
-		}
+    const handleKeydown = (e) => {
+        if (e.keyCode == 13 && response.toLowerCase() === countries[count].name.toLowerCase()) {
+            answer = ''
+            response = ''
+            $correct = [...$correct, countries[count].name];
+            count++
+        }
+    }
 
-			const showAnswer = () => {
-					answer = `The correct answer was ${countries[count].name}`
-					$correct = [] 
-					count = 0
-				}
+    const showAnswer = () => {
+        answer = `The correct answer was ${countries[count].name}`
+        $correct = []
+        count = 0
+    }
 </script>
 
 
 <svelte:head>
-	<title>Welcome</title>
+    <title>Welcome</title>
 </svelte:head>
 
-<svelte:window on:keydown={handleKeydown} />
+<svelte:window on:keydown={handleKeydown}/>
 
 <div class="font-mono space-y-4 p-12">
-	<p class="text-xl">learn countries a-z</p>
-	<input class="h-12 placeholder-gray-400 focus:outline-none" placeholder="enter country ..." bind:value={response} />
-	<p on:click={() => showAnswer()} class="text-blue-700 cursor-pointer">show answer</p>
-	<p>{answer}</p>
-	<ul class="text-sm grid grid-cols-3">
-		{#each $correct as country}
-			<li>{country}</li>
-		{/each}
-	</ul>
+    <p class="text-xl">learn countries a-z</p>
+    <input class="h-12 placeholder-gray-400 focus:outline-none" placeholder="enter country ..." bind:value={response}/>
+    <p on:click={() => showAnswer()} class="text-blue-700 cursor-pointer">show answer</p>
+    <p>{answer}</p>
+    <ul class="text-sm grid grid-cols-3">
+        {#each $correct as country}
+            <li>{country}</li>
+        {/each}
+    </ul>
 </div>
