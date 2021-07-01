cb8931fbaac4843abe7c164a0030133cd46901c6
ben@thedesigncollaborative.co <ben@thedesigncollaborative.co>
2021-07-01 15:46:35 +0100

    Finish frontend

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -20,20 +20,30 @@
 </script>
 
 <script>
+		import { writable } from "svelte/store"
 		export let countries;
+
+		let correct = writable([]) 
 
 		let response = '';
 		let count = 0;
+			let answer = ''
 
-			const handleKeydown = (e) => {
-					let keyCode = e.keyCode;
 
-					if (e.keyCode === 13 && response.toLowerCase() === countries[count].name.toLowerCase()) {
-							response = ''
-							count++
-						}
+		const handleKeydown = (e) => {
+			  if (e.keyCode == 13 && response.toLowerCase() === countries[count].name.toLowerCase()) {
+						answer = ''
+					response = ''
+					$correct = [...$correct, countries[count].name];
+					count++
 				}
+		}
 
+			const showAnswer = () => {
+					answer = `The correct answer was ${countries[count].name}`
+					$correct = [] 
+					count = 0
+				}
 </script>
 
 
@@ -43,5 +53,14 @@
 
 <svelte:window on:keydown={handleKeydown} />
 
-<input bind:value={response} />
-<p>{count}</p>
+<div class="font-mono space-y-4 p-12">
+	<p class="text-xl">learn countries a-z</p>
+	<input on:input={() => onInput()} class="placeholder-gray-400 focus:outline-none" placeholder="enter country ..." bind:value={response} />
+	<p on:click={() => showAnswer()} class="text-blue-700 cursor-pointer">show answer</p>
+	<p>{answer}</p>
+	<ul class="text-sm grid grid-cols-3">
+		{#each $correct as country}
+			<li>{country}</li>
+		{/each}
+	</ul>
+</div>
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,10 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 const config = {
 	mode: "jit",
 	purge: [
 		"./src/**/*.{html,js,svelte,ts}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			black: colors.black,
+			gray: colors.trueGray,
+		},
 	},
 	plugins: [],
 };
